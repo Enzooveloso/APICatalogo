98cf647cf0e4d201a802f09d1c5508cde1141d1b
Enzo Veloso <enzoveloso.2002@gmail.com>
2024-11-06 11:05:23 -0300

    Add project files.

---
+++ APICatalogo/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace APICatalogo.Models;
+
+public class Product
+{
+    public int ProductID { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public float Stock { get; set; }
+    public DateTime DateRegistration { get; set;}
+}
